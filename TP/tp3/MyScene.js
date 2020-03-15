ff6ddc8325515631d284dbdc0b7ec94b743194e9
Henrique <gamingwithapotato@gmail.com>
2020-03-15 17:47:48 +0000

    Fixed cube lighting issue

--- TP/tp3/MyScene.js
+++ TP/tp3/MyScene.js
@@ -129,7 +129,7 @@ class MyScene extends CGFscene {
         this.material4 = new CGFappearance(this);
         this.material4.setAmbient(0.8, 0.65, 0.45, 1.0);
         this.material4.setDiffuse(0.8, 0.65, 0.45, 1.0);
-        this.material4.setSpecular(0.8, 0.65, 0.45, 0.3);
+        this.material4.setSpecular(0.05, 0.05, 0.05, 1.0);
         this.material4.setShininess(10.0);
 
         // Custom material (can be changed in the interface)
