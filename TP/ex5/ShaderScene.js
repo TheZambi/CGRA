fbce898da4ea99a4bdc924574dd8f4121d58d76e
Ivo Saavedra <ivo_saavedra@outlook.com>
2020-03-27 18:12:29 +0000

    Finished water a)

--- TP/ex5/ShaderScene.js
+++ TP/ex5/ShaderScene.js
@@ -51,11 +51,20 @@ class ShaderScene extends CGFscene {
 		this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
 		this.appearance.setShininess(120);
 
-		this.texture = new CGFtexture(this, "textures/texture.jpg");
+		// this.texture = new CGFtexture(this, "textures/texture.jpg");
+		// this.appearance.setTexture(this.texture);
+		// this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+
+		// this.texture2 = new CGFtexture(this, "textures/FEUP.jpg");
+
+		this.texture = new CGFtexture(this, "textures/waterTex.jpg");
 		this.appearance.setTexture(this.texture);
 		this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.texture2 = new CGFtexture(this, "textures/FEUP.jpg");
+		this.texture2 = new CGFtexture(this, "textures/waterMap.jpg");
+
+
+		
 
 		// shaders initialization
 
@@ -70,13 +79,15 @@ class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
 			new CGFshader(this.gl, "shaders/ex1.vert", "shaders/ex1.frag"),
-			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/grayscale.frag")
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/grayscale.frag"),
+			new CGFshader(this.gl, "shaders/water.vert", "shaders/water.frag")
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
 		this.testShaders[4].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[5].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ uSampler2: 1 });
+		this.testShaders[11].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ timeFactor: 0 });
 
 
@@ -93,7 +104,8 @@ class ShaderScene extends CGFscene {
 			'Sepia': 7,
 			'Convolution': 8,
 			'Half yellow half blue' : 9,
-			'Grayscale' : 10
+			'Grayscale' : 10,
+			'Water' : 11
 		};
 
 		// shader code panels references
